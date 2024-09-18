782c93c8bd4bc1d04c069e88fe1d45c01c79315c
Giovanni Destratis <destratisgiovanni2@gmail.com>
2024-09-18 15:17:05 +0200

    form razor component create

--- blazorwebapp/obj/Debug/net8.0/blazorwebapp.AssemblyInfo.cs
+++ blazorwebapp/obj/Debug/net8.0/blazorwebapp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("blazorwebapp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6878c5575eeeead56fdaa140510ea299aa5f3ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c284371a7f0e8dfdee168f764962f59e863064a")]
 [assembly: System.Reflection.AssemblyProductAttribute("blazorwebapp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("blazorwebapp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
